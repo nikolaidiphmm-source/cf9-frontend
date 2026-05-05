6d681bce3a5b8862b072c8c998e14dffc004e550
nikolaidiphmm-source <nikolaidi.phmm@gmail.com>
2026-05-05 14:27:40 +0300

    feat: navbar finished

--- src/app/components/step7-list-group-menu/step7-list-group-menu.ts
+++ src/app/components/step7-list-group-menu/step7-list-group-menu.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-step7-list-group-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './step7-list-group-menu.html',
   styleUrl: './step7-list-group-menu.css',
 })
-export class Step7ListGroupMenu {}
+export class Step7ListGroupMenu {
+  menu = [
+    { text: "Step 2 Person Table", appLink:'person-table-example' },
+    { text: "Step 3 Component Input", appLink:'component-input-example' },
+    { text: "Step 4 @for Directive", appLink:'for-directive-example' },
+    { text: "Step 5 Event Bind", appLink:'event-bind-example' }
+  ]
+}
