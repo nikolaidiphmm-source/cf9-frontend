ab1f8d49194baf3b58d16507398da3956032df8d
nikolaidiphmm-source <nikolaidi.phmm@gmail.com>
2026-05-08 12:58:05 +0300

    feat: add login, logout

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { UserService } from '../../shared/services/user.service';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, MatIconModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  userService = inject(UserService);
+  user = this.userService.user;
+
+  logout(){
+    this.userService.logoutUser();
+  }
+}
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,6 +2,8 @@ import { effect, inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { Credentials, LoggedInUser } from '../interface/user-login.interface';
+import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
 
 const API_AUTH_URL = `${environment.apiURL}/api/auth`;
 
@@ -10,10 +12,21 @@ const API_AUTH_URL = `${environment.apiURL}/api/auth`;
 })
 export class UserService {
   http: HttpClient = inject(HttpClient);
+  router = inject(Router);
 
   user = signal<LoggedInUser | null>(null);
 
   constructor() {
+    const access_token = localStorage.getItem('access_token');
+    
+    if (access_token) {
+      const decodedToken = jwtDecode(access_token) as unknown as LoggedInUser;
+      this.user.set({
+        username: decodedToken.username,
+        email: decodedToken.email,
+        roles: decodedToken.roles
+      })
+    }
     effect(() => {
       if (this.user()){
         console.log("User Logged in:", this.user()?.username)
@@ -28,5 +41,11 @@ export class UserService {
       `${API_AUTH_URL}/login`, 
       data
     )
+  }
+
+  logoutUser(){
+    this.user.set(null);
+    localStorage.removeItem('access_token');
+    this.router.navigate(['user-login']);
   }
 }
