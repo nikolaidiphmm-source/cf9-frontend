b6280eba07adabde69ef603dcb07b8746aed2597
nikolaidiphmm-source <nikolaidi.phmm@gmail.com>
2026-05-08 12:23:11 +0300

    feat: add user login and signal variable

--- src/app/components/step13-user-login/step13-user-login.ts
+++ src/app/components/step13-user-login/step13-user-login.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/forms'
 import { Credentials, LoggedInUser } from '../../shared/interface/user-login.interface';
 import { UserService } from '../../shared/services/user.service';
+import { jwtDecode } from 'jwt-decode'
 
 @Component({
   selector: 'app-step13-user-login',
@@ -28,6 +29,15 @@ export class Step13UserLogin {
     this.userService.loginUser(credentials).subscribe({
       next: (response) => {
         console.log(response);
+        const access_token = response.token;
+        localStorage.setItem('access_token', access_token)
+        const decodedToken = jwtDecode(access_token) as unknown as LoggedInUser;
+        console.log("DECODED TOKEN>>", decodedToken);
+        this.userService.user.set({
+          username: decodedToken.username,
+          email: decodedToken.email,
+          roles: decodedToken.roles
+        })
       },
       error: (error) => {
         console.log(error);
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,7 +1,7 @@
-import { inject, Injectable } from '@angular/core';
+import { effect, inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { Credentials } from '../interface/user-login.interface';
+import { Credentials, LoggedInUser } from '../interface/user-login.interface';
 
 const API_AUTH_URL = `${environment.apiURL}/api/auth`;
 
@@ -10,6 +10,18 @@ const API_AUTH_URL = `${environment.apiURL}/api/auth`;
 })
 export class UserService {
   http: HttpClient = inject(HttpClient);
+
+  user = signal<LoggedInUser | null>(null);
+
+  constructor() {
+    effect(() => {
+      if (this.user()){
+        console.log("User Logged in:", this.user()?.username)
+      } else {
+        console.log('No user logged in');
+      }
+    })
+  }
 
   loginUser(data: Credentials) {
     return this.http.post<{token:string, user:{id:string, username:string}}> (
